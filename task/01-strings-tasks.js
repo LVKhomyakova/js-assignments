6f69d1fb14e8144c891a199a62576ce0bed0b04c
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-07-03 00:29:27 +0300

    feat: getCardId

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -273,7 +273,9 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    const suits = ['♣','♦','♥','♠'];
+    const ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    return ranks.indexOf(value.slice(0,-1)) + (suits.indexOf(value[value.length - 1]) * 13);
 }
 
 
